c0070566ea7a532dd7976e7677b5c47d958ea31a
jtcapsco-alt <jtcapsco@gmail.com>
2026-08-30 16:25:50 -0500

    Nuevo flujo de arranque: splash, bienvenida, carrusel y principios KOF

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "787640463" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "1572314771" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
