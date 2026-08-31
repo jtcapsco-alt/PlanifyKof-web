f6c56b791ed733f3f7c97561e13ac6e560d2ca1e
jtcapsco-alt <jtcapsco@gmail.com>
2026-08-31 00:53:50 -0500

    Ajustes de diseño del login

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "2907104346" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "597845843" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
