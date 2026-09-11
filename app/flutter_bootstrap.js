9593178a658ff8c601ef1389a68050cb37ea14c0
jtcapsco-alt <jtcapsco@gmail.com>
2026-09-11 01:44:47 -0500

    Linea 1, 3 y 4: puestos dinamicos, RM, celula 2 transversal, correccion de ausencias multi-linea

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "415034169" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "1024136325" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
