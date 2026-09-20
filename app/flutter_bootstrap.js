d481e67ed1889bc30d3d2c917832f5d308c99466
jtcapsco-alt <jtcapsco@gmail.com>
2026-09-20 13:18:53 -0500

    Arreglo carga de cronograma: FilePicker API 0.86

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "3292294005" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "4206266892" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
