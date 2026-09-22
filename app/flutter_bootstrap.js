fa5a807c3792c33d2ea3bc4b63b6669c4094371e
jtcapsco-alt <jtcapsco@gmail.com>
2026-09-21 21:11:51 -0500

    actualizacion de la app

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "2490317955" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "697627298" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
